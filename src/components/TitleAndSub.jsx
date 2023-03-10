b2f37ac9e6d628b3c8b78f6d343ac393800ba007
roman0f <92151944+roman0f@users.noreply.github.com>
2023-03-10 09:43:03 +0100

    Finish component

--- src/components/TitleAndSub.jsx
+++ src/components/TitleAndSub.jsx
@@ -4,7 +4,7 @@ function TitleAndSub() {
   return (
     <div className="TitleAndSub">
         <h1 id='Titolonissimo'>Webapp API Chuck Norris</h1>
-        <h6>Design di una pagina che utilizza la API di chucknorris.io per generare alla pressione di un pulsante una battuta del tipo che selezioni nel menu a tendina qui sotto.</h6>
+        <h6 id='subtitle'>Design di una pagina che utilizza la API di chucknorris.io per generare alla pressione di un pulsante una battuta del tipo che selezioni nel menu a tendina qui sotto.</h6>
     </div>
   )
 }
