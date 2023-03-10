67dbdb79f029a2215d729c2e4ed51a4d974785e8
roman0f <92151944+roman0f@users.noreply.github.com>
2023-03-10 13:12:15 +0100

    Finished CSS i hope so

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,7 +3,10 @@ import '../styles/DropDown.css'
 function DropDown({category}) {
   return (
     <div className="DropDown">
-       <select name="categories" id="categorisid"></select>
+       <select name="categories" id="categorisid">
+        <option value="a">"cia"</option>
+        
+       </select>
     </div>
   )
 }
