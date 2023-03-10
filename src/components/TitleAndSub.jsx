7919a91983052297a5f0eb0f985e8dc5551ba658
roman0f <92151944+roman0f@users.noreply.github.com>
2023-03-10 09:35:49 +0100

    Added first rule on css

--- src/components/TitleAndSub.jsx
+++ src/components/TitleAndSub.jsx
@@ -3,7 +3,7 @@ import '../styles/TitleAndSub.css'
 function TitleAndSub() {
   return (
     <div className="TitleAndSub">
-        <h1>Webapp API Chuck Norris</h1>
+        <h1 id='Titolonissimo'>Webapp API Chuck Norris</h1>
         <h6>Design di una pagina che utilizza la API di chucknorris.io per generare alla pressione di un pulsante una battuta del tipo che selezioni nel menu a tendina qui sotto.</h6>
     </div>
   )
